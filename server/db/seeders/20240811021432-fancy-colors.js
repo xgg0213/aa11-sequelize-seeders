fa80713f780714469793f3ce28c2972f207bc430
xgg0213 <m.eringan@gmail.com>
2024-08-10 19:18:23 -0700

    completed

---
+++ server/db/seeders/20240811021432-fancy-colors.js
@@ -0,0 +1,33 @@
+'use strict';
+const {Color} = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await Color.bulkCreate([
+    {name: "green"},
+    {name: "purple"}
+   ], {validate: true})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Colors', {
+      name: ["green", "purple"]
+    })
+  }
+};
